359848b2cdd61447aace9f974a89341f1fee6762
GABRIEL EDUARDO DE MOURA FRANCA <gabriel.franca0020015928@edu.pe.senac.br>
2025-11-11 15:25:22 -0300

    ajeitando a zona

--- controller.js
+++ controller.js
@@ -3,20 +3,77 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const users = [];
 
-app.post("/usuarios", (req, res) => {
+
+app.post("/usuarios", async (req, res) => {
   users.push(req.body);
 
+  await prisma.user.create({
+    data:{
+      email: req.body.email,
+      name : req.body.name,
+      age : req.body.age
+
+    }
+  })
 
   res.status(201).json(req.body);
 
 });
 
-app.get("/usuarios", (req, res) => {
+app.get("/usuarios", async(req, res) => {
+
+let users = []
+
+  if(req.query){
+    users = await prismas.user.findMany({
+      where: {}
+    })
+  } else {
+    users = await prisma.user.findMany()
+  }
+
+   const users = await prisma.user.findMany()
+
+
+
   res.status(200).json(users);
 });
 //porra
+
+
+
+app.put("/usuarios", async (req, res) => {
+  users.push(req.body);
+
+  await prisma.user.update({
+    where: {
+      id: req.params.id
+    },
+
+    data:{
+      email: req.body.email,
+      name : req.body.name,
+      age : req.body.age
+
+    }
+  })
+
+  res.status(201).json(req.body);
+})
+
+app.delete('/usuarios/:id'), async (req,res) =>{
+  await prisma.user.delete({
+    where: {
+      id: req.params.id
+    }
+
+  })
+
+  res.status(200).json({message: "usuarios"})
+   
+}
+
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
